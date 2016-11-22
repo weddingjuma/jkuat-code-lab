7979b14689023c0815d6d67eb6e3ec404dfb911d
Thomas Kioko <kiokotomas@gmail.com>
2016-11-22 22:59:34 +0300

    Create string util class
    This class contain helper methods to validate strings using regex patterns

---
+++ app/src/main/java/com/thomaskioko/jkuat_codelab/util/StringUtils.java
@@ -0,0 +1,42 @@
+package com.thomaskioko.jkuat_codelab.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Class contains helper methods to validate strings.
+ *
+ * @author kioko
+ */
+
+public class StringUtils {
+
+    private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})");
+
+    /**
+     * Helper method to validate E-mail address with regular expression
+     *
+     * @param emailAddress {@link String} E-mail Address
+     * @return {@link Boolean} True/False
+     */
+    public static boolean validate(String emailAddress) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailAddress);
+        return matcher.find();
+    }
+
+    /**
+     * Helper method to validate password with regular expression
+     *
+     * @param password password for validation
+     * @return true valid password, false invalid password
+     */
+    public static boolean validatePassword(final String password) {
+
+        Matcher matcher = PASSWORD_PATTERN.matcher(password);
+        return matcher.matches();
+
+    }
+}
